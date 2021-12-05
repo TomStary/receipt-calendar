57ba3f1bc04f2cbd03a047b4051aff982a63ed23
Tomáš Starý <tomas.stary@icloud.com>
2021-12-05 12:23:57 +0100

    devise and bootstrap init

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# Routes definitions
 Rails.application.routes.draw do
+  root to: 'application#index'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
